74c83832267b8ec59ec4a98ed1d64a3fa2943463
lilyjge <lilyjge@gmail.com>
2024-08-22 15:32:32 -0400

    renamed project, added validation for time intervals

--- commands/utility/help.js
+++ commands/utility/help.js
@@ -6,18 +6,18 @@ module.exports = {
 		.setDescription('displays all commands'),
 	async execute(interaction) {
         return interaction.reply(`
-            /addcal (number: calories to be added) - adds calories to todays current count \n
-            /addtogoal (number: calories to be added to goal) - increases calorie goal by given amount \n
-            /calnotif (true/false: whether daily msg is sent) - sets whether you get msg on if you achieved daily goal \n
-            /deletegoal - deletes calorie goal \n
-            /endstudy - ends study reminders \n
-            /help - displays all commands \n
-            /removecal (number: calories to be removed)- removes calories from today \n
-            /setcal (number: current calories today) - sets current calories today \n
-            /setgoal (number: daily calorie goal) - sets calorie goal \n
-            /setstudy (number: minutes between reminders) - sends study reminders with intervals of given minutes in between \n
-            /viewcal - views current calories for today \n
-            /viewgoal - views calorie goal
+/addcal (number: calories to be added) - adds calories to todays current count
+/addtogoal (number: calories to be added to goal) - increases calorie goal by given amount
+/calnotif (true/false: whether daily msg is sent) - sets whether you get msg on if you achieved daily goal
+/deletegoal - deletes calorie goal
+/endstudy - ends study reminders 
+/help - displays all commands
+/removecal (number: calories to be removed)- removes calories from today
+/setcal (number: current calories today) - sets current calories today 
+/setgoal (number: daily calorie goal) - sets calorie goal 
+/setstudy (number: minutes between reminders) (number: minutes you will study for) - sends study reminders with intervals of given minutes in between 
+/viewcal - views current calories for today 
+/viewgoal - views calorie goal
             `);
 	}
 };
--- commands/utility/setstudy.js
+++ commands/utility/setstudy.js
@@ -13,19 +13,19 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('setstudy')
 		.setDescription('starts study reminders')
-    .addNumberOption(option => 
+    .addIntegerOption(option => 
       option
         .setName("min")
         .setDescription("minutes between reminders")
         .setRequired(true))
-    .addNumberOption(option => 
+    .addIntegerOption(option => 
       option
         .setName("dur")
         .setDescription("how long to study for in minutes")),
 	async execute(interaction) {
 		const user = interaction.user.id;
-    const min = interaction.options.getNumber("min");
-    const dur = interaction.options.getNumber("dur");
+    const min = interaction.options.getInteger("min");
+    const dur = interaction.options.getInteger("dur");
     const endTime = new Date(Date.now() + 1000 * 60 * dur);
     let cur = schedule.scheduledJobs[user];
     if(cur) cur.cancel();
@@ -40,7 +40,7 @@ module.exports = {
     else{
       cur = schedule.scheduleJob(user, `*/${min} * * * *`, async function() {
         const data = await getapi(api_url);  
-        interaction.followUp(`if youre studying, take a break~ if not, go study ( ｡ •̀ ᴖ •́ ｡) \nremember, ${data[0].a} said "${data[0].q}"~ (from [Zen Quotes API](https://zenquotes.io/))`);
+        interaction.followUp(`<@${user}> if youre studying, take a break~ if not, go study ( ｡ •̀ ᴖ •́ ｡) \nremember, ${data[0].a} said "${data[0].q}"~ (from [Zen Quotes API](https://zenquotes.io/))`);
       });
     }
     return interaction.reply(res);
