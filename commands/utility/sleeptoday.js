6434dc307fc5a281f8dd0e776258fde79eb06e49
lilyjge <lilyjge@gmail.com>
2024-08-23 11:57:27 -0400

    can cancel reminders for today even when not currently reminding

--- commands/utility/sleeptoday.js
+++ commands/utility/sleeptoday.js
@@ -7,13 +7,18 @@ module.exports = {
 		.setDescription('ends sleep reminders for today'),
 	async execute(interaction) {
 		const user = interaction.user.username;
+        let today = schedule.scheduledJobs[user];
         let cur = schedule.scheduledJobs[user + "min"];
         if(cur){
             cur.cancel();
             return interaction.reply(`good night~ sleep well <3`);
         }
+        else if(today){
+            today.cancelNext(true);
+            return interaction.reply(`good job sleeping early! good night~ sleep well <3`);
+        }
         else{
-            return interaction.reply(`you're not receiving sleep reminders right now (?)`);
+            return interaction.reply(`you're not receiving sleep reminders (?)`);
         }
 	}
 };
